6cef0fb39fab6a14f962cbaaa327375c10c6c61d
Edenilson Ruiz <ruiz.edenilson@gmail.com>
2024-06-17 18:34:46 +0000

    fix: connections

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -46,14 +46,11 @@ export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
       const args:String = process.env.CHROME_ARGS || "";
 
       const wbot: Session = new Client({
-        session: sessionCfg,
-        authStrategy: new LocalAuth({clientId: 'bd_'+whatsapp.id}),
-        puppeteer: {
-          executablePath: process.env.CHROME_BIN || undefined,
-          // @ts-ignore
-          browserWSEndpoint: process.env.CHROME_WS || undefined,
-          args: args.split(' ')
-        }
+         authStrategy: new LocalAuth({clientId: 'bd_'+whatsapp.id}),
+         webVersionCache: {
+           type: 'remote',
+           remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+         }
       });
 
       wbot.initialize();
